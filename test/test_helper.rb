fc274b783f4630ea6ecad3511b65f887885ba647
Ben Brinckerhoff <ben@bbrinck.com>
2011-12-12 11:09:29 +0000

    Improve tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,20 +9,26 @@ require 'rack/perftools_profiler'
 
 class Test::Unit::TestCase
 
+  def assert_ok status, body
+    unless status == 200
+      raise RuntimeError, body
+    end
+  end
+
 end
 
 ITERATIONS = case RUBY_VERSION
-               # Ruby 1.9.x is so fast that we need to add extra iterations 
+               # Ruby 1.9.x is so fast that we need to add extra iterations
                # to get profiling data
              when /1\.9\../
-               350_000   
-             else 
+               350_000
+             else
                35_000
              end
 
 # From the Rack spec (http://rack.rubyforge.org/doc/files/SPEC.html) :
 # The Body must respond to each and must only yield String values. The Body should not be an instance of String.
-# ... The Body commonly is an Array of Strings, the application instance itself, or a File-like object. 
+# ... The Body commonly is an Array of Strings, the application instance itself, or a File-like object.
 
 class RackResponseBody
   include Test::Unit::Assertions
@@ -52,7 +58,7 @@ class TestApp
       end
       GC.start
     when /method2/
-      ITERATIONS.times do 
+      ITERATIONS.times do
         self.class.new.method2
       end
       GC.start
@@ -61,7 +67,7 @@ class TestApp
   end
 
   def method1
-    100.times do 
+    100.times do
       1+2+3+4+5
     end
   end
